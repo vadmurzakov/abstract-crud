65cfcaee2c72bb9dda76748f1fd3c4b8fa05a87d
vadmurzakov <vadmurzakov@gmail.com>
2018-12-14 15:52:03 +0300

    fix optional when save or update entity

--- src/main/java/com/github/vadmurzakov/abstractcrud/service/AbstractService.java
+++ src/main/java/com/github/vadmurzakov/abstractcrud/service/AbstractService.java
@@ -3,11 +3,9 @@ package com.github.vadmurzakov.abstractcrud.service;
 import com.github.vadmurzakov.abstractcrud.exception.EntityNotFoundException;
 import com.github.vadmurzakov.abstractcrud.model.Model;
 import com.github.vadmurzakov.abstractcrud.repository.EntityRepository;
-import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 public abstract class AbstractService<E extends Model, R extends EntityRepository<E>> implements EntityService<E> {
@@ -15,18 +13,13 @@ public abstract class AbstractService<E extends Model, R extends EntityRepositor
 	private R repository;
 
 	@Override
-	public Optional<E> save(E entity) {
-		return Optional.of(repository.save(entity));
+	public E save(E entity) {
+		return repository.save(entity);
 	}
 
 	@Override
-	public List<E> saveAll(List<E> entities) {
-		return Lists.newArrayList(repository.saveAll(entities));
-	}
-
-	@Override
-	public Optional<E> update(E entity) {
-		return Optional.of(repository.save(entity));
+	public E update(E entity) {
+		return repository.save(entity);
 	}
 
 	@Override
@@ -35,15 +28,8 @@ public abstract class AbstractService<E extends Model, R extends EntityRepositor
 	}
 
 	@Override
-	public List<E> getAll() {
-		return Lists.newArrayList(repository.findAll());
-	}
-
-	@Override
 	public Boolean delete(Serializable id) {
-		E entity = get(id).orElseThrow(
-				() -> new EntityNotFoundException(String.valueOf(id))
-		);
+		E entity = get(id).orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
 		repository.delete(entity);
 		return !repository.findById(id).isPresent();
 	}
--- src/main/java/com/github/vadmurzakov/abstractcrud/service/EntityService.java
+++ src/main/java/com/github/vadmurzakov/abstractcrud/service/EntityService.java
@@ -3,19 +3,14 @@ package com.github.vadmurzakov.abstractcrud.service;
 import com.github.vadmurzakov.abstractcrud.model.Model;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 public interface EntityService<E extends Model> {
-	Optional<E> save(E entity);
+	E save(E entity);
 
-	List<E> saveAll(List<E> entities);
-
-	Optional<E> update(E entity);
+	E update(E entity);
 
 	Optional<E> get(Serializable id);
-
-	List<E> getAll();
 
 	Boolean delete(Serializable id);
 }
